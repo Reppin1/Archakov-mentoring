412912196fa9f2978e5c6958a2f7e9bdce56c8a7
Andrey Krylov <anwing_2001@mail.ru>
2021-10-12 18:23:42 +0300

    Сделал урок  №2

--- src/tutorial-1/Form.jsx
+++ src/tutorial-1/Form.jsx
@@ -4,28 +4,26 @@ const Form = () => {
   let password = ''
 
   const changeValue = (event) => {
-    if(event.target.name === 'login') {
+    if (event.target.name === 'login') {
       login = event.target.value
-    }
-    else {
+    } else {
       password = event.target.value
     }
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    if(login.trim() && password.trim()) {
-      console.log(`Логин: ${login} Пароль: ${password}`)
+    if (login.trim() && password.trim()) {
+      console.log(`Логин: ${ login } Пароль: ${ password }`)
       event.target.reset()
-    }
-    else {
+    } else {
       alert('заполните поля')
     }
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={ handleSubmit }>
         <input name="login" type="text" onChange={ changeValue } placeholder="Введите логин" />
         <input name="password" type="password" onChange={ changeValue } placeholder="Введите пароль" />
         <button type="submit">Войти</button>
